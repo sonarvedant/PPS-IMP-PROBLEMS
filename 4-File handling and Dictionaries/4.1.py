b118b3a9431bd830adac0cf6f5224458341a6dfb
sonarvedant <sonarvedant22@gmail.com>
2026-04-29 23:02:06 +0530

    Update 4.1.py

---
+++ 4-File handling and Dictionaries/4.1.py
@@ -0,0 +1,11 @@
+"""
+File handling and Dictionaries
+FILE : file is named location on disc to store information 
+file have persistent data structure (memory stored permanantly in secondary memory) 
+every file is located by its path . and character \ used in pathname is called delimiter.
+
+absolute path : it is the complete path from the root directory to the file.
+relative path : it is the path from the current working directory to the file.
+
+
+"""
