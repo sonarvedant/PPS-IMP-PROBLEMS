e960f5b938c4844c4f5e995b11ad11786321cf25
sonarvedant <sonarvedant22@gmail.com>
2026-04-29 23:11:20 +0530

    ini

---
+++ 4-File handling and Dictionaries/4.0.notes.py
@@ -0,0 +1,24 @@
+"""
+File handling and Dictionaries
+FILE : file is named location on disc to store information 
+file have persistent data structure (memory stored permanantly in secondary memory) 
+every file is located by its path . and character \ used in pathname is called delimiter.
+
+absolute path : it is the complete path from the root directory to the file.
+relative path : it is the path from the current working directory to the file.
+
+Types of file 
+1] Text file : data present in form of characters 
+the text ascii file is simple file containign collection of characters that are readable to human 
+operations performed - opening file , reading file ,writing file , appending data to file 
+can be oped using texxt editor like notepad
+cannot be corrupted easily
+
+2] Binary file: data present in encoded form 
+image , audio , text data can be present 
+even if single bit is changed file is corrupted 
+cant read using text editor like notepad
+
+
+
+"""
