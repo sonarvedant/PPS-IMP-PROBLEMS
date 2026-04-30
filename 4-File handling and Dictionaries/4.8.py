b1b317a8e90c9c2abfd46b843ac07235817e645e
sonarvedant <sonarvedant22@gmail.com>
2026-04-30 23:20:19 +0530

    Create 4.8.py

---
+++ 4-File handling and Dictionaries/4.8.py
@@ -0,0 +1,8 @@
+#program to find a specific word in the file
+word = learning
+with open("practice.txt", "r") as f:
+    data = f.read()
+    if word in data:
+        print("word is present in file")
+    else:
+        print("word is not present in file")
