a388939199073621fa76dbf339ea6d3e54b86405
sonarvedant <sonarvedant22@gmail.com>
2026-04-30 23:09:53 +0530

    Create 4.7.py

---
+++ 4-File handling and Dictionaries/4.7.py
@@ -0,0 +1,10 @@
+#create a newfile "practice.txt" using python add th efollowing data
+"""
+Hi everyone 
+we are leaning file handling 
+using java
+i like java programing language
+
+"""
+#replacce java with python and save the file
+
