aece31f67fed79ea7eb64f5a4610deb0085c55e0
sonarvedant <sonarvedant22@gmail.com>
2026-04-30 23:18:15 +0530

    init

--- 4-File handling and Dictionaries/4.7.py
+++ 4-File handling and Dictionaries/4.7.py
@@ -1,10 +1,22 @@
-#create a newfile "practice.txt" using python add th efollowing data
-"""
+"""create a newfile "practice.txt" using python add th efollowing data
+
 Hi everyone 
 we are leaning file handling 
 using java
 i like java programing language
 
 """
-#replacce java with python and save the file
+#replace java with python and save the file
 
+with open("practice.txt", "r") as f:
+    data = f.read()
+    print("databefore editing is :", data)
+
+    data = data.replace("java", "python")
+    print("data after editing is :", data)
+
+# if we use w mode then we can overwrite the data in file 
+#means changes in main file 
+
+with open("practice.txt", "w") as f:
+    f.write(data)
