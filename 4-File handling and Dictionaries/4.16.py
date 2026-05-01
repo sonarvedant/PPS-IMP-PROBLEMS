d42ea836e4cfaf7a3f769974c382d5b46a9434aa
sonarvedant <sonarvedant22@gmail.com>
2026-05-01 21:26:48 +0530

    Update 4.16.py

--- 4-File handling and Dictionaries/4.16.py
+++ 4-File handling and Dictionaries/4.16.py
@@ -1,1 +1,10 @@
 #program to check whather the word is in  file and on which line
+f = open("4-File handling and Dictionaries\practice.txt", "r")
+word = "learning"
+data = True
+line = 1
+while data:
+    data = f.readline()
+    if word in data:
+        print(f"word is present in line {line}")
+    line += 1
