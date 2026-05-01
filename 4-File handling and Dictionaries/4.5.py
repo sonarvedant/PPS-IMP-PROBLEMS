0a1b8fb988e89b5420e5ef71f969efdad7f3329b
sonarvedant <sonarvedant22@gmail.com>
2026-05-01 14:09:57 +0530

    int

--- 4-File handling and Dictionaries/4.5.py
+++ 4-File handling and Dictionaries/4.5.py
@@ -1,2 +1,5 @@
 #write a python program to split the text line written in the file into words
 f = open("4-File handling and Dictionaries\practice.txt", "rt")
+line = f.readline()
+words = line.split()
+print(words)
--- 4-File handling and Dictionaries/4.6.py
+++ 4-File handling and Dictionaries/4.6.py
@@ -1,4 +1,11 @@
-#deleting a file 
-
-import os
-os.remove("filename")
+# write a program that counts number of lines and tabs character in file
+f = open("4-File handling and Dictionaries\practice.txt","rt")
+tabs = 0
+lines = 0
+for char in f.read():
+    if char == '\t':
+        tabs+=1
+    if char == '\n':
+        lines+=1
+print("tabs are :",tabs)
+print("lines are : ",lines)
