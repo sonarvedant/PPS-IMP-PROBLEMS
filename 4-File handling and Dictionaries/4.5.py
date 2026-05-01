70a7876ebcc38da9234d74662242926e8c9c38f1
sonarvedant <sonarvedant22@gmail.com>
2026-05-01 13:56:43 +0530

    i

--- 4-File handling and Dictionaries/4.5.py
+++
@@ -1,7 +0,0 @@
-#with syntax we can open a file and it will automatically close the file after the block of code is executed
-with open("4-File handling and Dictionaries\practice.txt", "r") as f:
-    data = f.read()
-    print(data)
-
-
-# as good programmer we close the file but in with it is not necessary 
