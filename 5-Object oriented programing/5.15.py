f3febfe755c49f2c1816f695ceca36e971bb4a34
sonarvedant <sonarvedant22@gmail.com>
2026-05-02 11:10:36 +0530

    Update 5.15.py

--- 5-Object oriented programing/5.15.py
+++ 5-Object oriented programing/5.15.py
@@ -5,3 +5,11 @@ class car:
 car1 = car()
 print(car1.color)
 print(car1.brand)
+
+car2 = car()
+print(car2.color)
+print(car2.brand)
+
+#here there is problem every car (car1, car2) har same color and brand
+#so in next program we will see how to add different value to different car 
+#that process is similar to concept argument and parameter in functions
