be27359b600116566002a297000de9442c7aae9d
sonarvedant <sonarvedant22@gmail.com>
2026-05-02 10:57:35 +0530

    Update 5.16.py

--- 5-Object oriented programing/5.16.py
+++ 5-Object oriented programing/5.16.py
@@ -1,8 +1,13 @@
 class student:
-    def __init__(self,fullname):
+    def __init__(self,fullname,marks):
         self.name = fullname 
-        
+        self.marks = marks
+        # here usings sel.name bcoz it is diff or every student 
+        # we can directly create variable if data commmon for all
         print("adding info via parameter and argument")
 
-s1 = student("karan")
-print(s1.name)
+s1 = student("karan",92)
+print(s1.name, s1.marks)
+
+s2 = student("arjun", 69)
+print(s2.name,s2.marks)
