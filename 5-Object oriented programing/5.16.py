1fdd21a7bd2ec916e8958a90c928040397ccca60
sonarvedant <sonarvedant22@gmail.com>
2026-05-02 11:01:19 +0530

    Update 5.16.py

--- 5-Object oriented programing/5.16.py
+++ 5-Object oriented programing/5.16.py
@@ -1,13 +1,14 @@
 class student:
+    # we can directly create variable if data commmon for all(eg - college name) above def__init__
+    college = "COEP"
     def __init__(self,fullname,marks):
         self.name = fullname 
         self.marks = marks
         # here usings sel.name bcoz it is diff or every student 
-        # we can directly create variable if data commmon for all
         print("adding info via parameter and argument")
 
 s1 = student("karan",92)
-print(s1.name, s1.marks)
+print(s1.name, s1.marks,s1.college)
 
 s2 = student("arjun", 69)
-print(s2.name,s2.marks)
+print(s2.name,s2.marks,s2.college)
