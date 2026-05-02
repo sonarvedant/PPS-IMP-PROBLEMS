cb1d3d9219567bb61ddead330e1ab1bd372a2fd6
sonarvedant <sonarvedant22@gmail.com>
2026-05-02 10:48:26 +0530

    i

--- 5-Object oriented programing/5.0.notes.py
+++ 5-Object oriented programing/5.0.notes.py
@@ -11,8 +11,9 @@ Encapsulation
 Firstly we made class
 and then object
 
-
-
+Consrtucter: it can be created if not createdd then it is createrd on its own
+def __init__(self,fulname):
+        print("   ")
 
 
 
