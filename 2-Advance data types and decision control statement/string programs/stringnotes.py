af0154169149a77a001db3b9abe3f5dd0b011df3
sonarvedant <sonarvedant22@gmail.com>
2026-05-10 00:18:31 +0530

    Create stringnotes.py

---
+++ 2-Advance data types and decision control statement/string programs/stringnotes.py
@@ -0,0 +1,27 @@
+"""
+
+
+
+
+
+if we want to pront next line just after pronting previous line then we use end=''
+
+
+strings are immutable i.e we cant assign value in existing string
+eg.
+str1 = "hello"   
+str1[1]= p
+not possible
+
+
+
+Inbuilt string methods and functions
+1] capitalize()
+2] count()
+3] find()
+4] index()
+5] isalpha()
+6] isdecimal()
+
+
+"""
